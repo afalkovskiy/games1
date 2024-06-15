860f4b6e45da946d55f9a6557731a10eec467995
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2024-06-15 00:21:33 -0600

    Add files via upload

---
+++ game10_list7fire.py
@@ -0,0 +1,198 @@
+# create animation
+import pygame
+
+pygame.init()
+
+def get_image(sheet, frame, width, height, scale, color1):
+    image = pygame.Surface((width, height)).convert_alpha()
+    image.blit(sheet,(0, 0), (frame*width, 0, 24, 24))
+    image = pygame.transform.scale(image, (width * scale, height * scale))
+    image.set_colorkey(color1)
+    # image = pygame.transform.flip(surface=image, flip_x=True, flip_y=False)
+    
+    
+    return image
+
+SCREEN_WIDTH = 700
+SCREEN_HEIGHT = 500
+BLACK = (0, 0, 0)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Alex game anim list7')
+
+sprite_sheet_image = pygame.image.load('img/doux4.png').convert_alpha()
+sprite_sheet_flame = pygame.image.load('img/flame_24x24.png').convert_alpha()
+
+BG = (200, 255, 200)
+bg_img = pygame.image.load("img/bg2.png")
+# rock_img = pygame.image.load("img/flame_48x48.png").convert_alpha()
+# rock_img = pygame.transform.scale(rock_img, (120,120))
+# rock_img.set_colorkey(BLACK)
+
+animation_list1 = []
+animation_steps1 = 4
+animation_list2 = []
+animation_steps2 = 10
+# to add animation_steps3 - jump, ...step4 - quick run
+animation_list3 = []
+animation_steps3 = 3
+
+animation_list4 = []
+animation_steps4 = 5
+
+animation_list5 = []
+animation_steps5 = 6
+
+animation_list6 = []
+animation_steps6 = 6
+
+last_update = pygame.time.get_ticks()
+animation_cooldown = 200
+frame = 0
+
+action = 1
+
+for i in range(animation_steps1):
+    animation_list1.append(get_image(sprite_sheet_image, i, 24, 24, 5, BLACK))
+
+for i in range(5,15):
+    animation_list2.append(get_image(sprite_sheet_image, i, 24, 24, 5, BLACK))
+
+# to add animatio_list3 - jump, ...list4 - quick run  
+for i in range(15,18):
+    animation_list3.append(get_image(sprite_sheet_image, i, 24, 24, 5, BLACK))  
+
+for i in range(19,24):
+    animation_list4.append(get_image(sprite_sheet_image, i, 24, 24, 5, BLACK)) 
+    
+for i in range(25,31):
+    animation_list5.append(get_image(sprite_sheet_image, i, 24, 24, 5, BLACK))
+
+for i in range(animation_steps6):
+    animation_list6.append(get_image(sprite_sheet_flame, i, 48, 48, 5, BLACK))
+
+run = True
+x=0
+y=200
+d=20
+dy=0
+
+xRect = 200
+yRect = 230
+flag = True
+# flagRun = False
+# flagSummersault = False
+
+
+
+while run:
+
+    # screen.fill(BG)
+    screen.blit(bg_img, (0,0))
+    # screen.blit(rock_img, (xRect,yRect-20))
+    screen.blit(animation_list6[frame%5], (xRect,yRect-40))
+
+    if action==1:
+        animation_list =  animation_list1
+        animation_cooldown = 200 
+
+    if action == 2:
+        animation_list =  animation_list2
+        animation_cooldown = 100
+
+    if action == 3:
+        animation_list =  animation_list3
+        animation_cooldown = 100
+
+    if action == 4:
+        animation_list =  animation_list4
+        animation_cooldown = 75 
+
+    if action == 5:
+        animation_list =  animation_list5
+        animation_cooldown = 200         
+
+    # pygame.draw.rect(screen, (0,100,0), pygame.Rect(xRect, yRect, 60, 60))
+   
+    current_time = pygame.time.get_ticks()
+    if current_time - last_update >= animation_cooldown:
+        frame +=1
+        if x>380:
+            flag = False
+        if x<0:
+            flag = True
+            # pygame.transform.rotate(animation_list[frame],180.)
+        
+        if flag==True:
+            x = x + d
+        else:
+            x = x -d   
+
+        if abs(x - xRect) < 60 and abs(y - yRect) < 40:
+            d = 0
+        else:
+            d = 20
+
+        y = y + dy      
+
+        if y >=200:
+            y = 200
+            dy = 0
+
+        if abs(x - xRect) >= 60 and abs(y - yRect) >= 40:
+            d = 20
+            # y = 200
+            dy = 20
+
+         
+
+        print("len anim list = ", len(animation_list))
+        if frame > len(animation_list)-1:
+            frame = 0
+
+        last_update = current_time
+
+
+    # print('frame = ', frame)
+    if flag:    
+        screen.blit(animation_list[frame], (x,y))
+    else:
+        img=animation_list[frame]
+        img = pygame.transform.flip(surface=img, flip_x=True, flip_y=False)
+        img.set_colorkey(BLACK)
+        screen.blit(img, (x,y))
+        # screen.blit(animation_list6[frame%5], (x,y))
+        #image = pygame.transform.flip(surface=image, flip_x=True, flip_y=False)
+
+    # for i in range(animation_steps):
+    #     screen.blit(animation_list[i], (60*i + 30, 80))
+	
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                flag = False
+            if event.key == pygame.K_RIGHT:
+                flag = True
+            if event.key == pygame.K_UP:
+                y = 120   
+            if event.key == pygame.K_r:
+                action = 2
+                frame = 0
+            if event.key == pygame.K_w:
+                action = 1
+                frame = 0 
+            if event.key == pygame.K_j:
+                action = 3
+                frame = 0 
+            if event.key == pygame.K_f:
+                action = 4
+                frame = 0 
+            if event.key == pygame.K_s:
+                action = 5
+                frame = 0                        
+
+    pygame.display.update()
+
+pygame.quit()
---
+++ game8_move_21.py
@@ -0,0 +1,91 @@
+# create animation
+import pygame
+
+pygame.init()
+
+def get_image(sheet, frame, width, height, scale, color1):
+    image = pygame.Surface((width, height)).convert_alpha()
+    image.blit(sheet,(0, 0), (frame*width, 0, 96, 96))
+    image = pygame.transform.scale(image, (width * scale, height * scale))
+    image.set_colorkey(color1)
+    # image = pygame.transform.flip(surface=image, flip_x=True, flip_y=False)
+    
+    
+    return image
+
+SCREEN_WIDTH = 1600
+SCREEN_HEIGHT = 900
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Alex game 8')
+
+# sprite_sheet_image = pygame.image.load('img/doux4.png').convert_alpha()
+sprite_sheet_image = pygame.image.load('img/peppa96.png').convert_alpha()
+
+BG = (200, 255, 200)
+BLACK = (0, 255, 0)
+
+animation_list = []
+animation_steps = 5
+last_update = pygame.time.get_ticks()
+animation_cooldown = 100
+frame = 0
+
+for i in range(animation_steps):
+    # animation_list.append(get_image(sprite_sheet_image, i, 24, 24, 5, BLACK))
+    animation_list.append(get_image(sprite_sheet_image, i, 96, 96, 3, BLACK))
+
+
+
+
+run = True
+x=0
+y=200
+d=20
+flag = True
+while run:
+
+	#update background
+    # screen.blit(frame_0, (0,0))
+    screen.fill(BG)
+   
+
+    current_time = pygame.time.get_ticks()
+    if current_time - last_update >= animation_cooldown:
+        frame +=1
+        if x>SCREEN_WIDTH - 100 or x<0:
+            d = d * (-1)
+            if flag:
+                flag = False
+            else:
+                flag = True
+            # pygame.transform.rotate(animation_list[frame],180.)
+            
+        x = x + d
+
+        if frame > len(animation_list)-1:
+            frame = 0
+
+        last_update = current_time
+
+
+    # print('frame = ', frame)
+    if flag:    
+        screen.blit(animation_list[frame], (x,y))
+    else:
+        img=animation_list[frame]
+        img = pygame.transform.flip(surface=img, flip_x=True, flip_y=False)
+        img.set_colorkey(BLACK)
+        screen.blit(img, (x,y))
+        #image = pygame.transform.flip(surface=image, flip_x=True, flip_y=False)
+
+    # for i in range(animation_steps):
+    #     screen.blit(animation_list[i], (60*i + 30, 80))
+	
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
